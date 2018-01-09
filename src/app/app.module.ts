5c9b9aee53eb471341921da3a4c15295201e3555
Lisa Forrest <lisam.forrest@libertymutual.com>
2018-01-09 15:39:01 -0500

    fix merge errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
-import { MatDialogModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './routing.module';
 import { DataService } from './data.service';
 import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
 import { StatusMessageComponent } from './status-message/status-message.component';
 import { CharityFormComponent } from './charity-form/charity-form.component';
-
 import { DonorFormComponent } from './donor-form/donor-form.component';
-
-
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { AppRoutingModule } from './routing/routing.module';
-import { FormsModule } from '@angular/forms';
 import { DonatedItemFormComponent } from './donated-item-form/donated-item-form.component';
-import { DataService } from './data.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { StatusMessageComponent } from './status-message/status-message.component'
+import {AppRoutingModule} from'./routing/routing.module';
+
 
 
 @NgModule({
@@ -45,7 +38,6 @@ import { StatusMessageComponent } from './status-message/status-message.componen
     BrowserModule,
     AppRoutingModule,
 
-    MatDialogModule,
     HttpModule,
     BrowserAnimationsModule,
     FormsModule
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { DonorFormComponent }   from '../donor-form/donor-form.component'; 
 import { HomeComponent }   from '../home/home.component';
 import { DonatedItemFormComponent } from '../donated-item-form/donated-item-form.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-  
+import { CharityFormComponent } from '../charity-form/charity-form.component';
+
+ 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent },
   { path: 'donatedItem/add', component: DonatedItemFormComponent },
   { path: 'dashboard', component: DashboardComponent},
-  { path: 'donor',  component: DonorFormComponent }
-  
+  { path: 'donor',  component: DonorFormComponent },
+  { path: 'charityForm/add', component: CharityFormComponent},
 
 
 ];
