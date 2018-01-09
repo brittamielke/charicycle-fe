bf60db0dbc637e97b80d1bf6183ee253ebff657f
Lisa Forrest <lisam.forrest@libertymutual.com>
2018-01-09 14:47:05 -0500

    charicycle add a charity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter}from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+ 
+  itemsList;
+
+  constructor(private dataService: DataService) {}
+
+  getDataFromService() {
+    this.dataService.getItemsList()
+      .subscribe(
+      itemsList => {
+
+        this.itemsList = itemsList[0];
+      }
+      )
+  }
+  ngOnInit() {
+    this.getDataFromService()
+  }
+} 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { MatDialogModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routing.module';
+import { DataService } from './data.service';
+import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
+import { StatusMessageComponent } from './status-message/status-message.component';
+import { CharityFormComponent } from './charity-form/charity-form.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteConfirmComponent,
+    StatusMessageComponent,  
+    CharityFormComponent,
+        
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatDialogModule,
+    HttpModule,
+    FormsModule
+   
   ],
-  providers: [],
+  entryComponents: [DeleteConfirmComponent],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
