e63f88c9fe13dd04f20ac68abb19f934868ea476
Britta Mielke <britta.mielke@libertymutual.com>
2018-01-10 11:34:15 -0500

    added needed thing functionality

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent }   from '../home/home.component';
 import { DonatedItemFormComponent } from '../donated-item-form/donated-item-form.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { CharityFormComponent } from '../charity-form/charity-form.component';
+import { NeededItemFormComponent } from '../needed-item-form/needed-item-form.component';
+
 
  
 const routes: Routes = [
@@ -15,12 +17,12 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'donor',  component: DonorFormComponent },
   { path: 'charityForm/add', component: CharityFormComponent},
-
-
+  { path: 'neededItem/add', component: NeededItemFormComponent },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+
 export class AppRoutingModule {}
