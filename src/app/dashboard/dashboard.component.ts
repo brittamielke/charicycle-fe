d37981cd20ccbb7f5ab718f1520ded7249374567
David Hall <david.hall@libertymutual.com>
2018-01-10 16:24:04 -0500

    made some enhancements to dashboard and delete functionality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,23 +17,36 @@ export class DashboardComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location) { }
 
-  getItems(endpoint: string) {
-    this.dataService.getRecords(endpoint)
+  getDonatedItems() {
+    this.dataService.getRecords('donatedItems')
       .subscribe(
       records => console.log(this.donatedItems = records),
       error => console.log(error));
   }
 
-  getNeededItems(endpoint: string) {
-    this.dataService.getRecords(endpoint)
+  getNeededItems() {
+    this.dataService.getRecords('neededItems')
       .subscribe(
       records => console.log(this.neededItems = records),
       error => console.log(error));
   }
+  deleteDonatedItem(id){
+    this.dataService.deleteRecord('donatedItems', id)
+      .subscribe(
+      records => this.getDonatedItems(),
+      error => console.log(error))
+  }
+
+  deleteNeededItem(id) {
+    this.dataService.deleteRecord('neededItems', id)
+      .subscribe(
+      records => this.getNeededItems(),
+      error => console.log(error))
+  }
 
   ngOnInit() {
-    this.getItems('donatedItems');
-    this.getNeededItems('neededItems')
+    this.getDonatedItems();
+    this.getNeededItems()
   }
 
 }
