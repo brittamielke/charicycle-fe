389e376989a0e765f04c8d43eae7b28bac6038fe
David Hall <david.hall@libertymutual.com>
2018-01-17 16:44:48 -0500

    pushing another minor change to redirect users after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DataService } from '../data.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,27 @@ import { DataService } from '../data.service'
 export class LoginComponent implements OnInit {
   successMessage;
   errorMessage;
+  user = null;
+  type;
 
-
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   login(loginForm: NgForm){
-    console.log(loginForm.value);
     this.dataService.login(loginForm.value)
       .subscribe(
       user => {this.successMessage = "Login Successful";
-              localStorage.setItem("user", JSON.stringify(user))},
+              this.user = user;
+        localStorage.setItem("user", JSON.stringify(user))
+        if (this.user.admin) {
+          this.router.navigate([`/admin`]);
+        }
+          else if (this.user.taxId) {
+            this.router.navigate([`/dashboard/charity/${this.user.id}`]);
+          } else {
+            this.router.navigate([`/dashboard/donor/${this.user.id}`]);
+          }
+          
+          },
       error => this.errorMessage = <any>error);
 
   }
