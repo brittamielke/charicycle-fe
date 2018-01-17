1d58b60881a2dd5c08c455bda1ce7ac1c297ae67
Beth Skwerski <Beth.Skwerski@libertymutual.com>
2018-01-17 13:19:42 -0500

    Adding button functionality to the datatables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { AdminComponent } from './admin/admin.component';
 import { MatDialogModule } from '@angular/material';
 import { DataTablesModule } from 'angular-datatables';
 import { DistanceDataService } from './google-distance.service';
+import { DonatedItemTableComponent } from './donated-item-table/donated-item-table.component';
 
 
 @NgModule({
@@ -49,6 +50,7 @@ import { DistanceDataService } from './google-distance.service';
     ManagecharitiesComponent,
     ManagedonorsComponent,
     AdminComponent,
+    DonatedItemTableComponent,
     
 
   ],
---
+++ src/app/donated-item-table/donated-item-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donated-item-table',
+  templateUrl: './donated-item-table.component.html',
+  styleUrls: ['./donated-item-table.component.css']
+})
+export class DonatedItemTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
